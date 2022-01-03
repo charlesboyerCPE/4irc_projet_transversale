305aff8437974eb4474c574b5287077047d08c2e
LaurineBoulio <laurine.boulio@gmail.com>
2022-01-03 16:55:05 +0100

    api get

--- web/ServeurWebEmergency/index.php
+++ web/ServeurWebEmergency/index.php
@@ -2,14 +2,102 @@
 require('./controleur/controleur.php');
 require('./controleur/api.php');
 
-$action = isset($_GET['action']) ? $_GET['action'] : NULL; // récupérer l'action navbar
+//$action = isset($_GET['action']) ? $_GET['action'] : NULL; // récupérer l'action navbar
 
 $params = explode('/', htmlspecialchars(rtrim($_GET['action'],'/')));
 //if(!empty($params[0])) test();
 //$params[0]();
 
-if($params[0] == "api"){
-   test();
+if($params[0] == "api"){  
+    
+    
+    /*
+     /api/feu/id/
+     /api/feux/
+    */
+   insert(1);
+
+   $request_method = $_SERVER["REQUEST_METHOD"];
+   switch($request_method){
+       case 'GET':
+            // Capteur par id
+            if($params[1]=="capteur"){
+                if(isset($params[2])){
+                    api_capteur($params[2]);
+                }
+                else{
+                    header("HTTP/1.0 405 Method Not Allowed");
+                    break;
+                }
+            }
+            // Capteurs
+            elseif($params[1]=="capteurs"){
+                if(!isset($params[2])){
+                    api_capteurs();
+                }
+                else{
+                    header("HTTP/1.0 404 Not Found");
+                    break;
+                }
+            }
+            //FEU PAR ID
+            elseif($params[1]=="feu"){
+                if(isset($params[2])){
+                    api_feu($params[2]);
+                }
+                else{
+                    header("HTTP/1.0 405 Method Not Allowed");
+                    break;
+                }
+            }
+           // FEUX
+            elseif($params[1]=="feux"){
+                if(!isset($params[2])){
+                    api_feux();
+                }
+                else{
+                    header("HTTP/1.0 404 Not Found");
+                    break;
+                }
+            }
+           // CAMION PAR ID
+            elseif($params[1]=="camion"){
+                if(isset($params[2])){
+                    api_camion($params[2]);
+                }
+                else{
+                    header("HTTP/1.0 405 Method Not Allowed");
+                    break;
+                }
+            }
+           // CAMIONS
+            elseif($params[1]=="camions"){
+                if(!isset($params[2])){
+                    api_camions();
+                }
+                else{
+                    header("HTTP/1.0 404 Not Found");
+                    break;
+                }
+            }
+
+
+           else{
+                header("HTTP/1.0 404 Not Found");
+                break;
+            }
+            break;
+            default:
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+   }
+   exit();
+
+
+
+
+
+
 }
 elseif($params[0] == "getCarte"){
     carte();
--- web/ServeurWebEmergency/modele/BDD.php
+++ web/ServeurWebEmergency/modele/BDD.php
@@ -1,6 +1,26 @@
 <?php
+
+abstract class BDD{
+      private $host = "localhost";
+      private $db_name = "database_emergency";
+      private $username = "root";
+      private $password = "";
+
+      protected $_connexion;
+
+      public function getConnection(){
+          $this->_connexion = null;
+          try{
+              $this->_connexion = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+              $this->_connexion->exec("set names utf8");
+          }catch(PDOException $exception){
+              echo "Erreur de connexion : " . $exception->getMessage();
+          }
+      }   
+}
 		
-	function connectDb(){
+/*
+            function connectDb(){
             $host = 'localhost'; // ou localhost
             $user = 'root';      // ou login
             $pwd = '';      // ou xxxxxx
@@ -20,7 +40,7 @@
                   exit('Erreur : '.$e->getMessage());
             }
       }
-
+*/
 
  /*
   $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
