81df8a34f0252e2c3ad3096cbb15924039779e04
LaurineBoulio <laurine.boulio@gmail.com>
2022-01-06 17:37:12 +0100

    map js icones

--- web/ServeurWebEmergency/public/js/map.js
+++ web/ServeurWebEmergency/public/js/map.js
@@ -28,13 +28,26 @@ function afficheMap(listeCapteurs, listeFeux, listeCamions, listePompiers){
     }).addTo(map);
 
     //Création des icones
+
+    var camionIcon = L.icon({
+        iconUrl: './public/img/camion.png',
+       shadowUrl: '',
+    
+        iconSize:     [30, 30], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+
     var capteurIcon = L.icon({
         iconUrl: './public/img/capteur.png',
        shadowUrl: '',
     
-        iconSize:     [50, 50], // size of the icon
+        iconSize:     [30, 30], // size of the icon
         shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
         shadowAnchor: [4, 62],  // the same for the shadow
         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
@@ -43,22 +56,43 @@ function afficheMap(listeCapteurs, listeFeux, listeCamions, listePompiers){
         iconUrl: './public/img/feu.png',
        shadowUrl: '',
     
-        iconSize:     [50, 50], // size of the icon
+        iconSize:     [30, 30], // size of the icon
         shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
         shadowAnchor: [4, 62],  // the same for the shadow
         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
+
+    var pompierIcon = L.icon({
+        iconUrl: './public/img/pompier.png',
+       shadowUrl: '',
+    
+        iconSize:     [30, 30], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+
     //L.marker([45.764043, 4.835659], {icon: feuIcon}).bindPopup("Feu").addTo(map);
-/*
-    for (var i = 0; i < listeCapteurs.length; i++) {
-        L.marker([listeFeux[i].coordonnee_x, listeFeux[i].coordonnee_y], {icon: capteurIcon}).bindPopup("Capteur").addTo(map);
-    }
-*/
-    for (var i = 0; i < listeFeux.length; i++) {
-        L.marker([45.764043, 4.835659], {icon: feuIcon}).bindPopup("Feu").addTo(map);
+
+    for (var i = 0; i < listeCamions.length; i++) {
+        L.marker([listeCamions[i].coordonnee_x, listeCamions[i].coordonnee_y], {icon: camionIcon}).bindPopup("Camion").addTo(map);
     }
 
+    for (var i = 0; i < listeCapteurs.length; i++) {
+        L.marker([listeCapteurs[i].coordonnee_x, listeCapteurs[i].coordonnee_y], {icon: capteurIcon}).bindPopup("Capteur").addTo(map);
+    }
+
+    for (var i = 0; i < listeFeux.length; i++) {
+        L.marker([listeFeux[i].coordonnee_x, listeFeux[i].coordonnee_y], {icon: feuIcon}).bindPopup("Feu").addTo(map);
+    }
+
+    for (var i = 0; i < listePompiers.length; i++) {
+        L.marker([listePompiers[i].coordonnee_x, listePompiers[i].coordonnee_y], {icon: pompierIcon}).bindPopup("Pompier").addTo(map);
+    }
+/*
     //Capteurs sous forme de cercles
     for (var i = 0; i < listeCapteurs.length; i++) {
         //console.log("listeCapteurs" + listeCapteurs[i].id_capteur);
@@ -69,7 +103,7 @@ function afficheMap(listeCapteurs, listeFeux, listeCamions, listePompiers){
             radius: 200//500
         }).addTo(map);
     }
-  /*  
+ */ /*  
 
     for (var i = 0; i < listeFeux.length; i++) {
          L.circle([listeFeux[i].coordonnee_x, listeFeux[i].coordonnee_y], {
